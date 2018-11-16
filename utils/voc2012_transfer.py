8151b697431b42a05fee1cbdbe566fbfe8754326
Jojozzc <zzc185@foxmail.com>
2018-11-16 09:02:24 +0800

    voc2012 transfer completed

--- utils/voc2012_transfer.py
+++ utils/voc2012_transfer.py
@@ -1,7 +1,58 @@
 # Select useful from voc2012 dataset.
 import os
+import shutil
 
 VOC2012_BASE_PATH = '/run/media/kele/Data/VOCtrainval_11-May-2012/VOCdevkit/VOC2012'
+
+
+def copy_file_by_list_rename(source_dir, target_dir, file_list, class_id):
+	for file in file_list:
+		if file:
+			file = file + '.jpg'
+			shutil.copy(os.path.join(source_dir, file), os.path.join(target_dir, str(class_id) + '_'+ file))
+
+def gen_voc_data():
+	voc2012_base_path = VOC2012_BASE_PATH
+	main_dir = os.path.join(voc2012_base_path, 'ImageSets/Main')
+	rank_reid_datasource_voc_base_dir = '/run/media/kele/DataSSD/Code/multi-task/rank-reid/datasource/voc-data'
+	img_source_dir = os.path.join(voc2012_base_path, 'JPEGImages')
+
+	train_num = 1500
+	test_num = 2000
+	objs = ['cat', 'cow', 'dog', 'horse', 'sheep']
+	for i in range(len(objs)):
+		train_txt_path = os.path.join(main_dir, objs[i] + '_train.txt')
+		val_txt_path = os.path.join(main_dir, objs[i] + '_val.txt')
+		train_list = []
+		test_list = []
+		with open(train_txt_path) as txt:
+			line = txt.readline()
+			count = 0
+			while line and count < train_num:
+				line = line.strip().split(' ')[0]
+				print(line)
+				count = count + 1
+				train_list.append(line)
+				line = txt.readline()
+			copy_file_by_list_rename(
+				source_dir=img_source_dir,
+				target_dir=os.path.join(rank_reid_datasource_voc_base_dir, 'train'),
+				file_list=train_list,
+				class_id=i)
+
+		with open(val_txt_path) as txt:
+			line = txt.readline()
+			count = 0
+			while line and count < test_num:
+				count = count + 1
+				line = line.strip().split(' ')[0]
+				test_list.append(line)
+				line = txt.readline()
+			copy_file_by_list_rename(
+				source_dir=img_source_dir,
+				target_dir=os.path.join(rank_reid_datasource_voc_base_dir, 'test'),
+				file_list=test_list,
+				class_id=i)
 
 
 def count_animal_seg_cls(voc2012_base):
@@ -38,7 +89,7 @@ def count_animal_seg_cls(voc2012_base):
 	with open(seg_train_txt_path) as file:
 		line  = file.readline()
 		while line:
-			line = line.split(' ')[0]
+			line = line.strip()
 			seg_train_set.add(line)
 			line = file.readline()
 
@@ -46,13 +97,14 @@ def count_animal_seg_cls(voc2012_base):
 		line = file.readline()
 		while line:
 			# line = line.split(' ')[0]
+			line = line.strip()
 			seg_trainval_set.add(line)
 			line = file.readline()
 
 	with open(seg_val_txt_path) as file:
 		line = file.readline()
 		while line:
-			line = line.split(' ')[0]
+			line = line.strip()
 			# print(line)
 			seg_val_set.add(line)
 			line = file.readline()
@@ -67,7 +119,7 @@ def count_animal_seg_cls(voc2012_base):
 			line = file.readline()
 			while line:
 				line =	line.split(' ')[0]
-				# print(line)
+				print(line)
 				if line in seg_train_set:
 					train_set.add(line)
 				elif line in seg_trainval_set:
@@ -110,13 +162,14 @@ def count_animal_seg_cls(voc2012_base):
 		trainval_file = open(os.path.join(anm_save_dir, 'trainval.txt'), mode='w+')
 		val_file = open(os.path.join(anm_save_dir, 'val.txt'), mode='w+')
 		for idx in train_set:
-			train_file.writelines(idx)
+			train_file.write(idx + '\n')
 		for idx in trainval_set:
-			trainval_file.writelines(idx)
+			trainval_file.write(idx + '\n')
 		for idx in val_set:
-			val_file.writelines(idx)
+			val_file.write(idx + '\n')
 
 
 if __name__ == '__main__':
-	count_animal_seg_cls(voc2012_base=VOC2012_BASE_PATH)
+	# count_animal_seg_cls(voc2012_base=VOC2012_BASE_PATH)
+	gen_voc_data()
 	print('ok')
